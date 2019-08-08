e3b3d1957073be0cf9b59c1484e0290e7e1787f3
llucasmota <lucas.o.mota@gmail.com>
2019-08-08 10:18:12 -0300

    Foi adicionada validação de schema do objeto enviado na atualização do usuário e na criação

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -4,6 +4,18 @@ import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: { message: 'Validation fails' } });
+    }
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
     if (userExists) {
@@ -17,6 +29,25 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string()
+        .email()
+        .required(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+    });
+    /**
+     * validando se o schema é válido
+     */
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: { message: 'Validation fails' } });
+    }
+
     const user = await User.findByPk(req.userId);
     /**
      * email and password need different validation, but
