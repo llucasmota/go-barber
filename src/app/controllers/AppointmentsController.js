1262eca3fcafdf3d4e8b45052323e4247bc0c53d
llucasmota <lucas.o.mota@gmail.com>
2019-09-04 07:49:59 -0300

    Adicionado as seguintes validações: Verificar se há um appointment com as mesmas características no banco e se a data informada é uma data passada

--- src/app/controllers/AppointmentsController.js
+++ src/app/controllers/AppointmentsController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { isBefore, startOfHour, parseISO } from 'date-fns';
 import Appointment from '../models/Appointments';
 import User from '../models/User';
 
@@ -23,6 +24,32 @@ class AppointmentsController {
       return res.status(401).json({
         error: { message: 'You can only create Appointments with providers' },
       });
+    }
+    /**
+     * startOfHour ignora os minutos e analisa apenas a hora e a data, o parseISO
+     * transforma o valor em um tipo date do JS.
+     * O if verifica o valor passado é menor que o atual
+     */
+    const hour = startOfHour(parseISO(date));
+    if (isBefore(hour, new Date())) {
+      return res
+        .status(400)
+        .json({ error: { message: 'Past dates aren`t permitted' } });
+    }
+    /**
+     * Verificando se há um agendamento com as mesmas características
+     */
+    const checkRepeatAppoint = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date,
+      },
+    });
+    if (checkRepeatAppoint) {
+      return res
+        .status(400)
+        .json({ error: { message: 'Appointment date is not available' } });
     }
     const appointment = await Appointment.create({
       user_id: req.userId,
