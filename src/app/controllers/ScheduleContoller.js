2d63ad46a262b26ff0cc0ebb79be0c66d22e1964
llucasmota <lucas.o.mota@gmail.com>
2019-09-09 19:23:19 -0300

    Ajuste ao verificar se usuário é um provider

--- src/app/controllers/ScheduleContoller.js
+++ src/app/controllers/ScheduleContoller.js
@@ -3,14 +3,19 @@ import Appointment from '../models/Appointments';
 
 class ScheduleController {
   async index(req, res) {
-    const user = await User.findOne({ where: { id: req.userId } });
-    if (!user.provider === true) {
+    const user = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+    /**
+     * Check if user is provider
+     */
+    if (!user) {
       return res
         .status(401)
-        .json({ error: { message: 'only providers can view' } });
+        .json({ error: { message: 'Only providers can view' } });
     }
     const schedule = await Appointment.findAll({
-      where: { provider_id: user.id },
+      where: { provider_id: user.id, canceled_at: null },
       order: ['date'],
       attributes: ['id', 'date'],
       include: [
