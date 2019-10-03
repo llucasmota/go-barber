7ddff129b2e205bcade3ebb5ba81735139554c5b
llucasmota <lucas.o.mota@gmail.com>
2019-10-03 09:30:07 -0300

    Foram adicionadas dependencias do express-handlebars e do handlebars para suportar os arquivos hbs, bem como suas bibliotecas, a fim de serem utilizados no envio do e-mail de cancelamento. Foi codificado o envio de e-mail para o cancelamento também

--- src/app/controllers/AppointmentsController.js
+++ src/app/controllers/AppointmentsController.js
@@ -137,6 +137,11 @@ class AppointmentsController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -176,7 +181,14 @@ class AppointmentsController {
     await Mail.sendMail({
       to: `${appointment.provider.name}<${appointment.provider.email}>`,
       subject: 'Agendamento cancelado',
-      text: 'Há um novo cancelamento',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', às' HH:mm'h'", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
